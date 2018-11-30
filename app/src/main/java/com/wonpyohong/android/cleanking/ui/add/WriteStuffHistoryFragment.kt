f09d4c9351586ba28c571cdad64d53adcaaae3c2
Wonpyo Hong <deblan2@naver.com>
2018-11-30 20:56:54 +0900

    WriteStuffHistoryFragment 내용을 대부분 viewModel 로 이동.

--- app/src/main/java/com/wonpyohong/android/cleanking/ui/add/WriteStuffHistoryFragment.kt
+++ app/src/main/java/com/wonpyohong/android/cleanking/ui/add/WriteStuffHistoryFragment.kt
@@ -1,35 +1,23 @@
 package com.wonpyohong.android.cleanking.ui.add
 
 import android.annotation.SuppressLint
+import android.arch.lifecycle.Observer
 import android.databinding.BindingAdapter
 import android.databinding.DataBindingUtil
 import android.databinding.ObservableArrayList
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.support.v7.widget.RecyclerView
-import android.support.v7.widget.helper.ItemTouchHelper
-import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import com.beloo.widget.chipslayoutmanager.ChipsLayoutManager
-import com.beloo.widget.chipslayoutmanager.SpacingItemDecoration
 import com.wonpyohong.android.cleanking.R
 import com.wonpyohong.android.cleanking.base.BaseFragment
 import com.wonpyohong.android.cleanking.databinding.FragmentWriteStuffHistoryBinding
 import com.wonpyohong.android.cleanking.room.stuff.Category
 import com.wonpyohong.android.cleanking.room.stuff.Stuff
-import com.wonpyohong.android.cleanking.room.stuff.StuffDatabase
-import com.wonpyohong.android.cleanking.room.stuff.StuffHistory
-import com.wonpyohong.android.cleanking.support.RxDayDataSetChangedEvent
-import com.wonpyohong.android.cleanking.support.recyclerview.DragHelperCallback
-import com.wonpyohong.android.cleanking.support.recyclerview.ItemTouchHelperAdapter
-import io.reactivex.Completable
-import io.reactivex.Single
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
-import org.threeten.bp.LocalDateTime
 
 @BindingAdapter("bind:categoryItem")
 fun bindCategoryItem(recyclerView: RecyclerView, categoryList: ObservableArrayList<Category>) {
@@ -50,48 +38,38 @@ class WriteStuffHistoryFragment: BaseFragment() {
 
     private lateinit var binding: FragmentWriteStuffHistoryBinding
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        observeEvents()
+    }
+
+    private fun observeEvents() {
+        viewModel.getActionSignal().observe(this, Observer {
+            when (it is WriteStuffHistoryViewModel.ACTION) {
+                it == WriteStuffHistoryViewModel.ACTION.CATEGORY_NOT_SELECTED -> {
+                    AlertDialog.Builder(context!!)
+                        .setMessage("카테고리를 선택하셔야 합니다")
+                        .show()
+                }
+                it == WriteStuffHistoryViewModel.ACTION.STUFF_NOT_SELECTED -> {
+                    AlertDialog.Builder(context!!)
+                        .setMessage("세부 항목을 선택하셔야 합니다")
+                        .show()
+                }
+                it == WriteStuffHistoryViewModel.ACTION.FINISH -> {
+                    activity?.finish()
+                }
+            }
+        })
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding = DataBindingUtil.bind(view)!!
         binding.viewModel = viewModel
-        binding.writeStuffHistoryFragment = this
-        initCategoryRecyclerView()
-        initStuffRecyclerView()
-    }
 
-    private fun initCategoryRecyclerView() {
-        val categoryAdapter = CategoryAdapter(viewModel)
-        initRecyclerViewCommon(binding.categoryRecyclerView, categoryAdapter)
-
-        StuffDatabase.getInstance().getCategoryDao().getAllCategoryList().subscribe {
-            viewModel.categoryList.clear()
-            viewModel.categoryList.addAll(it)
-        }
-    }
-
-    private fun initStuffRecyclerView() {
-        val stuffAdapter = StuffAdapter(viewModel)
-        initRecyclerViewCommon(binding.stuffRecyclerView, stuffAdapter)
-
-        viewModel.selectedCategory.addOnPropertyChanged {selectedObservableCategory ->
-            StuffDatabase.getInstance().getStuffDao().getStuffList(selectedObservableCategory.get()!!.categoryId)
-                .subscribe {list ->
-                    viewModel.stuffList.clear()
-                    viewModel.stuffList.addAll(list)
-                } }
-
-    }
-
-    private fun initRecyclerViewCommon(recyclerView: RecyclerView, adapter: RecyclerView.Adapter<*>) {
-        adapter.setHasStableIds(true)
-        recyclerView.adapter = adapter
-
-        recyclerView.layoutManager = ChipsLayoutManager.newBuilder(context!!).build()
-        recyclerView.addItemDecoration(
-            SpacingItemDecoration(20, 20)
-        )
-
-        val touchHelper = ItemTouchHelper(DragHelperCallback(adapter as ItemTouchHelperAdapter))
-        touchHelper.attachToRecyclerView(recyclerView)
+        viewModel.initCategoryRecyclerView(binding.categoryRecyclerView, ChipsLayoutManager.newBuilder(context!!).build())
+        viewModel.initStuffRecyclerView(binding.stuffRecyclerView, ChipsLayoutManager.newBuilder(context!!).build())
     }
 
     override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater) {
@@ -101,75 +79,9 @@ class WriteStuffHistoryFragment: BaseFragment() {
     @SuppressLint("CheckResult")
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.menu_write_history) {
-            return writeHistory()
+            return viewModel.writeHistory()
         }
 
         return super.onOptionsItemSelected(item)
     }
-
-    private fun writeHistory(): Boolean {
-        if (viewModel.selectedCategory.get() == null) {
-            AlertDialog.Builder(context!!)
-                .setMessage("카테고리를 선택하셔야 합니다")
-                .show()
-
-            return true
-        }
-
-        if (viewModel.selectedStuff.get() == null) {
-            AlertDialog.Builder(context!!)
-                .setMessage("세부 항목을 선택하셔야 합니다")
-                .show()
-
-            return true
-        }
-
-        val stuff = StuffHistory(0, LocalDateTime.now().minusHours(4).toLocalDate().toString(),
-            viewModel.selectedCategory.get()!!.categoryName,
-            viewModel.selectedStuff.get()!!.stuffName)
-
-        Single.just(1)
-            .subscribeOn(Schedulers.io())
-            .map {
-                StuffDatabase.getInstance().getStuffHistoryDao().insert(stuff)
-            }
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({ exerciseId ->
-                RxDayDataSetChangedEvent.sendEvent(RxDayDataSetChangedEvent.DayDataSetChanged())
-
-                activity?.finish()
-            }, {
-                it.printStackTrace()
-            })
-
-        return true
-    }
-
-    fun addStuff() {
-        viewModel.selectedCategory.get()?.let {
-            val newStuff = Stuff(
-                0,
-                it.categoryId,
-                binding.newStuffEditText.text.toString()            // two-way binding이 필요할 듯
-            )
-
-            Completable.fromAction {
-                StuffDatabase.getInstance().getStuffDao().insert(newStuff)
-            }.subscribeOn(Schedulers.io())
-                .subscribe()
-
-        } ?:
-        AlertDialog.Builder(context!!)              // event 로 notify 해야 한다 Rx?
-            .setMessage("카테고리를 선택하셔야 합니다")
-            .show()
-    }
-
-    fun <T: android.databinding.Observable> T.addOnPropertyChanged(callback: (T) -> Unit) =
-        addOnPropertyChangedCallback(
-            object: android.databinding.Observable.OnPropertyChangedCallback() {
-                override fun onPropertyChanged(
-                    observable: android.databinding.Observable?, i: Int) =
-                    callback(observable as T)
-            })
-
 }
--- app/src/main/java/com/wonpyohong/android/cleanking/ui/add/WriteStuffHistoryViewModel.kt
+++ app/src/main/java/com/wonpyohong/android/cleanking/ui/add/WriteStuffHistoryViewModel.kt
@@ -1,9 +1,26 @@
 package com.wonpyohong.android.cleanking.ui.add
 
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.LiveDataReactiveStreams
 import android.databinding.ObservableArrayList
 import android.databinding.ObservableField
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.helper.ItemTouchHelper
+import com.beloo.widget.chipslayoutmanager.ChipsLayoutManager
+import com.beloo.widget.chipslayoutmanager.SpacingItemDecoration
 import com.wonpyohong.android.cleanking.room.stuff.Category
 import com.wonpyohong.android.cleanking.room.stuff.Stuff
+import com.wonpyohong.android.cleanking.room.stuff.StuffDatabase
+import com.wonpyohong.android.cleanking.room.stuff.StuffHistory
+import com.wonpyohong.android.cleanking.support.RxDayDataSetChangedEvent
+import com.wonpyohong.android.cleanking.support.recyclerview.DragHelperCallback
+import com.wonpyohong.android.cleanking.support.recyclerview.ItemTouchHelperAdapter
+import io.reactivex.Completable
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.processors.PublishProcessor
+import io.reactivex.schedulers.Schedulers
+import org.threeten.bp.LocalDateTime
 
 class WriteStuffHistoryViewModel {
     var selectedCategory = ObservableField<Category>()
@@ -11,6 +28,50 @@ class WriteStuffHistoryViewModel {
 
     var categoryList = ObservableArrayList<Category>()
     var stuffList = ObservableArrayList<Stuff>()
+
+    var newStuffName = ObservableField<String>()
+
+    private val actionSignal: PublishProcessor<ACTION> = PublishProcessor.create()
+
+    fun getActionSignal(): LiveData<ACTION> {
+        return LiveDataReactiveStreams.fromPublisher(actionSignal)
+    }
+
+    fun initCategoryRecyclerView(categoryRecyclerView: RecyclerView, chipsLayoutManager: ChipsLayoutManager) {
+        val categoryAdapter = CategoryAdapter(viewModel)
+        initRecyclerViewCommon(categoryRecyclerView, categoryAdapter, chipsLayoutManager)
+
+        StuffDatabase.getInstance().getCategoryDao().getAllCategoryList().subscribe {
+            viewModel.categoryList.clear()
+            viewModel.categoryList.addAll(it)
+        }
+    }
+
+    fun initStuffRecyclerView(stuffRecyclerView: RecyclerView, chipsLayoutManager: ChipsLayoutManager) {
+        val stuffAdapter = StuffAdapter(viewModel)
+        initRecyclerViewCommon(stuffRecyclerView, stuffAdapter, chipsLayoutManager)
+
+        viewModel.selectedCategory.addOnPropertyChanged {selectedObservableCategory ->
+            StuffDatabase.getInstance().getStuffDao().getStuffList(selectedObservableCategory.get()!!.categoryId)
+                .subscribe {list ->
+                    viewModel.stuffList.clear()
+                    viewModel.stuffList.addAll(list)
+                } }
+
+    }
+
+    private fun initRecyclerViewCommon(recyclerView: RecyclerView, adapter: RecyclerView.Adapter<*>, chipsLayoutManager: ChipsLayoutManager) {
+        adapter.setHasStableIds(true)
+        recyclerView.adapter = adapter
+
+        recyclerView.layoutManager = chipsLayoutManager
+        recyclerView.addItemDecoration(
+            SpacingItemDecoration(20, 20)
+        )
+
+        val touchHelper = ItemTouchHelper(DragHelperCallback(adapter as ItemTouchHelperAdapter))
+        touchHelper.attachToRecyclerView(recyclerView)
+    }
 
     fun onCategoryClicked(category: Category) {
         categoryList.forEach {
@@ -26,5 +87,71 @@ class WriteStuffHistoryViewModel {
         }
         stuff.onClicked()
         selectedStuff.set(stuff)
+    }
+
+    fun addStuff() {
+        viewModel.selectedCategory.get()?.let {
+            if (!newStuffName.get().isNullOrEmpty()) {
+                val newStuff = Stuff(
+                    0,
+                    it.categoryId,
+                    newStuffName.get()!!
+                )
+
+                Completable.fromAction {
+                    StuffDatabase.getInstance().getStuffDao().insert(newStuff)
+                }.subscribeOn(Schedulers.io())
+                    .subscribe()
+            }
+        } ?:
+            actionSignal.offer(ACTION.CATEGORY_NOT_SELECTED)
+    }
+
+    fun writeHistory(): Boolean {
+        if (viewModel.selectedCategory.get() == null) {
+            actionSignal.offer(ACTION.CATEGORY_NOT_SELECTED)
+
+            return true
+        }
+
+        if (viewModel.selectedStuff.get() == null) {
+            actionSignal.offer(ACTION.STUFF_NOT_SELECTED)
+
+            return true
+        }
+
+        val stuff = StuffHistory(0, LocalDateTime.now().minusHours(4).toLocalDate().toString(),
+            viewModel.selectedCategory.get()!!.categoryName,
+            viewModel.selectedStuff.get()!!.stuffName)
+
+        Single.just(1)
+            .subscribeOn(Schedulers.io())
+            .map {
+                StuffDatabase.getInstance().getStuffHistoryDao().insert(stuff)
+            }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ exerciseId ->
+                RxDayDataSetChangedEvent.sendEvent(RxDayDataSetChangedEvent.DayDataSetChanged())
+
+                actionSignal.offer(ACTION.FINISH)
+            }, {
+                it.printStackTrace()
+            })
+
+        return true
+    }
+
+    private fun <T: android.databinding.Observable> T.addOnPropertyChanged(callback: (T) -> Unit) =
+        addOnPropertyChangedCallback(
+            object: android.databinding.Observable.OnPropertyChangedCallback() {
+                override fun onPropertyChanged(
+                    observable: android.databinding.Observable?, i: Int) =
+                    callback(observable as T)
+            })
+
+    enum class ACTION {
+        CATEGORY_NOT_SELECTED,
+        STUFF_NOT_SELECTED,
+        FINISH,
     }
 }
